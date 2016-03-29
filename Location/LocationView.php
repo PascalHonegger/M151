9e99de5030c81fa2c7201b6e28fd594ae839abac
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2016-03-29 16:26:03 +0200

    View for locations done

--- Location/LocationView.php
+++ Location/LocationView.php
@@ -1,10 +1,10 @@
-<div id="content">
+<div id="content" class="Location-Div" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
+     xmlns="http://www.w3.org/1999/html">
     <form action="LocationInput.php" method="POST" enctype="multipart/form-data">
-        <input type="text" name="creator"/><br>
-        <input type="text" name="name" /><br>
-        <input type="text" name="description"/><br>
-        <input type="text" name="position"/><br>
-        <label>File: </label><input type="file" name="userfile[]" id="userfile[]" /><br>
-        <input type="submit" />
+        <label class="LocationLabel"> Name <br> </label><input type="text" name="name" class="RegisterInput"/></label><br>
+        <label class="LocationLabel"> Beschreibung <br> <input type="text" name="description" class="RegisterInput"/></label><br>
+        <label class="LocationLabel"> Position <br> <input type="text" name="position" class="RegisterInput"/></label><br>
+        <label class="LocationLabel"l>File <br> </label><input type="file" name="userfile[]" id="userfile[]" class="LocationInput"/></label><br>
+        <input type="submit" class="LocationButton" />
     </form>
 </div>
