72457aef24f9e8ad2380c035e7bce1b5931b0c52
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2016-03-29 16:28:55 +0200

    # 2  View for locations done

--- Location/LocationView.php
+++ Location/LocationView.php
@@ -1,6 +1,6 @@
 <div id="content" class="Location-Div" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
      xmlns="http://www.w3.org/1999/html">
-    <form action="LocationInput.php" method="POST" enctype="multipart/form-data">
+    <form action="../Location/LocationInput.php" method="POST" enctype="multipart/form-data">
         <label class="LocationLabel"> Name <br> </label><input type="text" name="name" class="RegisterInput"/></label><br>
         <label class="LocationLabel"> Beschreibung <br> <input type="text" name="description" class="RegisterInput"/></label><br>
         <label class="LocationLabel"> Position <br> <input type="text" name="position" class="RegisterInput"/></label><br>
