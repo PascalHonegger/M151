323090c773a89de845083fb0bf2ef4457946220f
Alain Keller <alain.keller@edu.tbz.ch>
2016-04-05 15:45:57 +0200

    small css changes

--- Home/Header.php
+++ Home/Header.php
@@ -19,7 +19,7 @@
     <div id="navigation">
         <ul id="navigationList">
             <li ><a class="navigationButton" id="currentSite" href="Home.php">Startseite</a></li>
-            <li ><a class="navigationButton" href="#">Entdecken</a></li>
+            <li ><a class="navigationButton" href="../Discover/Discover.php">Entdecken</a></li>
             <li ><a class="navigationButton" href="../Event/Event.php">Neuer Event</a></li>
         </ul>
     </div>
