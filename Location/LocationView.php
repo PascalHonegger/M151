ec27bae22ae40ae95fe6526d5edda0cbbd0d99ca
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2016-04-05 14:59:48 +0200

    # 2  Deletet Home.php in Event Folder

--- Location/LocationView.php
+++ Location/LocationView.php
@@ -1,16 +1,9 @@
 <div id="content" class="Location-Div" xmlns="http://www.w3.org/1999/html">
     <form action="../Location/LocationInput.php" method="POST" enctype="multipart/form-data">
-        <label class="LocationLabel" for="name"> Name</label><br><input type="text" id="name" name="name"
-                                                                        class="RegisterInput"/><br>
-        <label class="LocationLabel" for="description"> Beschreibung</label><br><input type="text" id="description"
-                                                                                       name="description"
-                                                                                       class="RegisterInput"/><br>
-        <label class="LocationLabel" for="position"> Position</label> <br> <input type="text" name="position"
-                                                                                  id="position"
-                                                                                  class="RegisterInput"/><br>
-        <label class="LocationLabel" for="userfile[]">File <br> </label><input type="file" name="userfile[]"
-                                                                               id="userfile[]"
-                                                                               class="LocationInput"/><br>
-        <input type="submit" class="LocationButton" />
+        <label class="LocationLabel" for="name"> Name</label><br><input type="text" id="name" name="name" class="RegisterInput"/><br>
+        <label class="LocationLabel" for="description"> Beschreibung</label><br><input type="text" id="description" name="description" class="RegisterInput"/><br>
+        <label class="LocationLabel" for="position"> Position</label> <br> <input type="text" name="position" id="position" class="RegisterInput"/><br>
+        <label class="LocationLabel" for="userfile[]">File <br> </label><input type="file" name="userfile[]" id="userfile[]" class="LocationInput"/><br>
+        <button class="LocationButton" onclick="createLocation()" >Absenden</button>
     </form>
 </div>
